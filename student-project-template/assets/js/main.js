8586d1a62f863c203f1a4c5efd9e89a9e2a2d2f4
gontugithub <gs.alvarez8@gmail.com>
2025-09-23 14:15:28 +0200

    boton added with click event

--- student-project-template/assets/js/main.js
+++ student-project-template/assets/js/main.js
@@ -5,3 +5,11 @@ const title = document.getElementById('title');
 if(title){
 	title.textContent = 'BIENVENIDO A MI PAGINA 2';
 }
+
+const btn = document.getElementById('btn-action');
+
+if (btn) {
+	btn.addEventListener('click', () => {
+		alert('🚀 Botón pulsado correctamente');
+	});
+}
